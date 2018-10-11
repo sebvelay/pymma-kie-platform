d454ebc3e2ef2840270412d45cd8866d98ccad38
Nicolas Héron <nicolas.heron@pymma-software.com>
2018-10-11 11:30:45 +0200

    Maven dependency problem+admin consoel as war

--- drools-framework-admin-console/src/main/java/org/chtijbug/drools/console/DroolsSpringBootConsoleApplication.java
+++ drools-framework-admin-console/src/main/java/org/chtijbug/drools/console/DroolsSpringBootConsoleApplication.java
@@ -4,6 +4,8 @@ package org.chtijbug.drools.console;
 import org.chtijbug.drools.console.service.util.ApplicationContextProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
@@ -14,7 +16,13 @@ import org.springframework.context.annotation.PropertySource;
 //@ComponentScan({"com.assurfleet.front.courtier.petiteflotte.maj","com.assurfleet.jms.services"})
 @PropertySource("classpath:application.properties")
 @ImportResource("classpath:applicationContext.xml")
-public class DroolsSpringBootConsoleApplication {
+public class DroolsSpringBootConsoleApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DroolsSpringBootConsoleApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(DroolsSpringBootConsoleApplication.class, args);
     }
