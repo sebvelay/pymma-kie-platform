8be9dabaccecc4633993de145bbc9770f5e4675f
guillaume <guillaume.hochart@pymma-software.com>
2019-02-01 18:54:06 +0100

    modif diverse log

--- drools-framework-admin-console-parent/drools-framework-admin-console/src/main/java/org/chtijbug/drools/console/vaadinComponent/leftMenu/Action/ActionLogging.java
+++ drools-framework-admin-console-parent/drools-framework-admin-console/src/main/java/org/chtijbug/drools/console/vaadinComponent/leftMenu/Action/ActionLogging.java
@@ -4,8 +4,14 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
+import org.chtijbug.drools.console.service.IndexerService;
+import org.chtijbug.drools.console.service.util.AppContext;
 import org.chtijbug.drools.console.vaadinComponent.ComponentPerso.DialogPerso;
+import org.chtijbug.drools.console.vaadinComponent.ComponentPerso.TextFieldPerso;
 import org.chtijbug.drools.console.vaadinComponent.Squelette.SqueletteComposant;
 import org.chtijbug.drools.console.vaadinComponent.componentView.AddRuntime;
 import org.chtijbug.drools.console.vaadinComponent.componentView.DefineProject;
@@ -13,16 +19,48 @@ import org.chtijbug.drools.console.vaadinComponent.componentView.GridActionLoggi
 import org.chtijbug.drools.console.vaadinComponent.componentView.GridLogging;
 import org.chtijbug.drools.console.view.LoggingView;
 import org.chtijbug.drools.indexer.persistence.model.BusinessTransactionPersistence;
+import org.springframework.data.domain.PageRequest;
+
+import java.util.List;
 
 public class ActionLogging extends VerticalLayout {
 
     private Button viewAction;
 
+    private TextFieldPerso transactionIdSearch;
+
+    private IndexerService indexerService;
 
     public ActionLogging(LoggingView loggingView){
 
+        indexerService= AppContext.getApplicationContext().getBean(IndexerService.class);
+
         setClassName("leftMenu-global-action");
 
+        transactionIdSearch=new TextFieldPerso("Search by TransactionId","",VaadinIcon.SEARCH.create());
+        transactionIdSearch.getTextField().setValueChangeMode(ValueChangeMode.EAGER);
+        transactionIdSearch.getTextField().addValueChangeListener(textFieldStringComponentValueChangeEvent -> {
+
+            try {
+
+                if (textFieldStringComponentValueChangeEvent.getValue().isEmpty()) {
+                    loggingView.getTitle().setText("Logging : ");
+                    loggingView.getGridLogging().setDataProvider(indexerService.getBusinessTransactionPersistenceRepository().findAll(new PageRequest(0, 100)).getContent());
+                } else {
+                    List<BusinessTransactionPersistence> b = indexerService.getBusinessTransactionPersistenceRepository().findAllByTransactionId(textFieldStringComponentValueChangeEvent.getValue(), new PageRequest(0, 100));
+
+                    if (b != null) {
+                        loggingView.getTitle().setText("Logging : " + textFieldStringComponentValueChangeEvent.getValue());
+                        loggingView.getGridLogging().setDataProvider(b);
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+                transactionIdSearch.getTextField().setValue("");
+                Notification.show("Valeur invalide");
+            }
+        });
+        add(transactionIdSearch);
 
         viewAction =new Button("View Details", VaadinIcon.INFO.create());
         viewAction.setClassName("leftMenu-global-button");
@@ -37,12 +75,14 @@ public class ActionLogging extends VerticalLayout {
 
             if(b!=null) {
                 DialogPerso dialog = new DialogPerso();
-                dialog.add(new Label(b.getTransactionId()));
+                dialog.add(new Label("Transaction Id : "+b.getTransactionId()));
                 dialog.add(new GridActionLogging(b.getId()));
                 dialog.open();
             }
 
         });
+
+
     }
     private boolean isActive(Button button){
         return button.getClassNames().contains("active");
